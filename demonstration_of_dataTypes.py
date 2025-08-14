a63550b463801fe094d9e02a50bd316bac66a03f
vinland-voyager <akjindividual@gmail.com>
2025-08-14 10:33:37 +0530

    Practical 1: Demonstrating Data Types in Python

---
+++ demonstration_of_dataTypes.py
@@ -0,0 +1,20 @@
+intVar = 27
+floatVar = 8.95
+complexVar = 3 + 5j
+strVar = "hello"
+boolVar = True
+listVar = [1, 2, 3]
+setVar = {2, 3, 3, 4, 4, "name", "name", "test", "test"}
+tupleVar = (3, 4, 5.8)
+dictVar = {"Name": "21", "Age": 21}
+
+print(intVar, "| This is", type(intVar), "data type.")
+print(floatVar, "| This is", type(floatVar), "data type.")
+print(complexVar, "| This is", type(complexVar), "data type.")
+print(strVar, "| This is", type(strVar), "data type.")
+print(boolVar, "| This is", type(boolVar), "data type.")
+print(listVar, "| This is", type(listVar), "data type.")
+print(setVar, "| This is", type(setVar), "data type.")
+print(tupleVar, "| This is", type(tupleVar), "data type.")
+print(dictVar, "| This is", type(dictVar), "data type.")
+
